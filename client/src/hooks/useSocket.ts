439f9cbc85baa4c207fcd4c220fc069c5b23c0d4
Amna Sabahat <amnasabahat6@gmail.com>
2026-09-20 16:12:55 +0500

    Fix socket URL to read from env var

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -2,10 +2,9 @@ import {io, Socket} from 'socket.io-client';
 
 let socket : Socket | null = null
 
-export function useSocket(){
-    if(!socket){
-       socket =  io('http://localhost:5000')
+export function useSocket() {
+    if (!socket) {
+        socket = io(import.meta.env.VITE_SOCKET_URL ?? 'http://localhost:5000');
     }
-
-    return socket
+    return socket;
 }
